1f772ce7e4058bcc9065237b72c24d17edfa46ff
zjr <me@zjr.io>
2013-04-01 21:46:48 -0400

    Don't watch & then annoyingly compile the include folder.

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,7 +33,7 @@ module.exports = function (grunt) {
 				tasks: ['coffee:test']
 			},
 			compass: {
-				files: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
+				files: ['<%= yeoman.app %>/styles/*.{scss,sass}'],
 				tasks: ['compass']
 			},
 			jade: {
