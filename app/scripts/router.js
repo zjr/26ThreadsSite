2e79c3369b2abac86453307c2f6616e63f1d2629
zjr <me@zjr.io>
2013-04-08 16:11:59 -0400

    Router for video creates new video view.

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,19 +1,22 @@
-define(['backbone'], function (Backbone) {
+define(['backbone', 'views/video'], function (Backbone, VideoView) {
     'use strict';
 
 	var AppRouter = Backbone.Router.extend({
 
 		routes: {
-			"thread/:id":      "showThread",  // #thread/11
-			"thread/:id/play": "showThread",  // #thread/11/play
-			"thread/:id/info": "showThread"   // #thread/11/info
+			'thread/:id':       'showThread',  // #thread/11
+			'thread/:id/video': 'showVideo',   // #thread/11/video
+			'thread/:id/info':  'showThread'   // #thread/11/info
 		},
 
-		showThread: function (id) {
-			//
+		showVideo: function (id) {
+			App.videoView = new VideoView({
+				el: $('#vModal'),
+				model: App.threadCol.get(id)
+			});
 		}
 
 	});
 
-	return AppRouter; 
+	return AppRouter;
 });
