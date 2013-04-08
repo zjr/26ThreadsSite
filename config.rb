e5eaa86d33a5a8ad6d9f28c21f58cb3f42ca7bab
zjr <me@zjr.io>
2013-04-08 15:55:40 -0400

    Add config.rb until I add Sass watch to Gruntfile.

---
+++ config.rb
@@ -0,0 +1,8 @@
+project_path = "app"
+sass_dir = "styles"
+css_dir = "styles"
+images_dir = "assets/images"
+fonts_dir = "assets/fonts"
+javascripts_dir = "scripts"
+additional_import_paths = ["components"]
+sass_options = {:sourcemap => true}
