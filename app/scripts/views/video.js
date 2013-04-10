1439d1395b32475ff4dea179cd3e5e6239ef898f
zjr <me@zjr.io>
2013-04-10 13:09:40 -0400

    Modal size on load and addtnl specifications.

--- app/scripts/views/video.js
+++ app/scripts/views/video.js
@@ -9,8 +9,25 @@ define(['backbone', 'text!templates/video.html', 'modal'], function (Backbone, v
 		},
 
 		render: function () {
+			this.resizeModal();
+
 			this.$el.html(this.template(this.model.attributes));
-			this.$el.modal();
+
+			// Stop the Soundtrack
+			App.audioView.specialStop();
+
+			this.$el.modal({
+				zIndex: 2,
+				closeText: 'x'
+			});
+		},
+
+		resizeModal: function () {
+			var dWidth = $(document).width();
+			var mWidth = dWidth / 2;
+			var aspect = 0.5625;
+
+			this.$el.width(mWidth).height(mWidth * aspect);
 		},
 
 		close: function () {
