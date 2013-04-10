533b0417973512690766ce36bf100fbfe31f6c42
zjr <me@zjr.io>
2013-04-10 13:12:11 -0400

    Route to "/#more-info" creates new xModal view.

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,4 +1,4 @@
-define(['backbone', 'views/video'], function (Backbone, VideoView) {
+define(['backbone', 'views/video', 'views/xmodal'], function (Backbone, VideoView, XModalView) {
     'use strict';
 
 	var AppRouter = Backbone.Router.extend({
@@ -6,13 +6,20 @@ define(['backbone', 'views/video'], function (Backbone, VideoView) {
 		routes: {
 			'thread/:id':       'showThread',  // #thread/11
 			'thread/:id/video': 'showVideo',   // #thread/11/video
-			'thread/:id/info':  'showThread'   // #thread/11/info
+			'thread/:id/info':  'showThread',  // #thread/11/info
+			'more-info':        'moreInfo'     // #more-info
 		},
 
 		showVideo: function (id) {
 			App.videoView = new VideoView({
 				el: $('#vModal'),
 				model: App.threadCol.get(id)
+			});
+		},
+
+		moreInfo: function () {
+			App.xModalView = new XModalView({
+				el: $('#xModal')
 			});
 		}
 
