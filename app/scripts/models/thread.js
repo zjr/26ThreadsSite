70ac745bf1e8e7230fc4ba2ec58070dc16ad5901
zjr <me@zjr.io>
2013-04-15 12:22:51 -0400

    Meh... reordering.

--- app/scripts/models/thread.js
+++ app/scripts/models/thread.js
@@ -1,15 +1,16 @@
 define(['backbone'], function (Backbone) {
+	'use strict';
 
 	var Thread = Backbone.Model.extend({
 		defaults: {
-			"title": null,
-			"subTitle": null,
-			"description": null,
-			"video": {
-				"url": null,
-				"locked": false,
-				"release": null,
-				"vimeoId": null
+			'title': null,
+			'subTitle': null,
+			'description': null,
+			'video': {
+				'url': null,
+				'vimeoId': null,
+				'locked': false,
+				'release': null
 			}
 		}
 	});
